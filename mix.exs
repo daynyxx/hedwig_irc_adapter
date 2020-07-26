d8400442b254cea7e84701de990bbea66789660c
cbdh2024 <cbdh2024@gmail.com>
2020-07-26 02:03:01 -0400

    init

---
+++ mix.exs
@@ -0,0 +1,52 @@
+defmodule HedwigIrc.Mixfile do
+  use Mix.Project
+  @description """
+    An IRC adapter for Hedwig
+  """
+
+  def project do
+    [
+      app: :hedwig_irc,
+      version: "0.1.0",
+      elixir: "~> 1.2",
+      name: "hedwig_irc_adapter",
+      description: @description,
+      package: package(),
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+    ]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type "mix help compile.app" for more information
+  def application do
+    [applications: [:logger, :hedwig, :exirc]]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type "mix help deps" for more examples and options
+  defp deps do
+    [
+      {:hedwig, "~> 1.0.1"},
+      {:exirc, "~> 2.0.0"},
+      {:ex_doc, "~> 0.18", only: :dev},
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Chris Holmes"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/cbdh2024/hedwig_irc_adapter"},
+    ]
+  end
+end
